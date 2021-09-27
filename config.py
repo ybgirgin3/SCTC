961be926feae0b5822587ea488422b1aecf235a8
ybgirgin3 <ybgirgin3@gmail.com>
2021-09-27 11:55:28 +0300

    save image added

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+import pathlib
+
+home = pathlib.Path.home()
+
+
+
 tess_path = '/usr/bin/tesseract'
+save_path = f"{home}/Pictures"
--- getSS.py
+++ getSS.py
@@ -3,27 +3,48 @@ import tempfile as tmp
 from config import *
 import numpy as np
 import subprocess
+import argparse
 import cv2
 import os
 
 import notify2 as nt2
 
-fn = os.path.join(tmp.gettempdir(), 'temp_ss.png')
+
 
 def show_notif(init_str, n1, n2):
     nt2.init(init_str)
     n = nt2.Notification(n1, n2)
     n.show()
 
+def pather(mode: bool):
+    if mode:
+        fn = os.path.join(f"{save_path}", 'temp_ss.png')
+    elif not mode:
+        fn = os.path.join(tmp.gettempdir(), 'temp_ss.png')
 
-def getSS():
+    return fn
+
+
+
+def getSS(fn):
     # full file name
     # create file in tempdir
+
     os.system(f'scrot --line mode=edge -s -o -f  {fn}')
 
 if __name__ == '__main__':
-    # show notification
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--save", help="Save Screenshot to local drive", action="store_true")
+    args = vars(parser.parse_args())
+
+    fn = pather(args['save'])
+
+    ## show notification
     show_notif("SS to Clipboard for GNU/Linux", "You can draw the rectangle area, Please make sure target text can readable in your ScreenShot", f"SS path: {fn}")
-    getSS()
-    ret = ittMain(fn, tess_path)
-    show_notif(init_str="SS to Clipboard for GNU/Linux", n1=f"{ret}", n2="copied to clipboard")
+
+    getSS(fn)
+    if fn is not None:
+        ret = ittMain(fn, tess_path)
+    show_notif(init_str="SS to Clipboard for GNU/Linux", n1=f"{ret}", n2="done")
+
+    #print(args['save'])
